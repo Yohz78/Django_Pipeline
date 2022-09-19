02859e4ab2145dc33b8bdca5954693294824cbc6
LeoGx <lgarrigoux@gmail.com>
2022-09-19 20:32:54 +0200

    Refactoring mostly done

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Leo\Desktop\OC\Projet_13\Python-OC-Lettings-FR\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
